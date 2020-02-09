c4e26ccd0e3a8e74666297b47634363357f38f1a
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-08 17:24:50 -0800

    Implement basic User crud test

--- test/test.js
+++ test/test.js
@@ -2,22 +2,73 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const URI = 'http://localhost:3000'
 const expect = chai.expect
+const random = max => Math.floor(Math.random() * Math.floor(max))
+const id = random(25)
 
 chai.use(chaiHttp)
 
 describe('Users', () => {
-  //GET
-  describe('GET users/1', () => {
-    it('Should return a status 200', done => {
-      chai.request(URI)
-        .get('/users/1')
-        .end((err, res) => {
-          expect(err).to.be.null
-          expect(res).to.have.status(200)
-          done()
-        })
-    })
-  })
+    //GET
+    describe('GET users/:id', () => {
+      it('Should return a status 200', done => {
 
-  // POST
+        chai.request(URI)
+          .get(`/users/${id}`)
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            done()
+          })
+      })
+    })
+
+    // POST
+    describe('POST users/', () => {
+      it('Should return a status 201', done => {
+        const username = 'user' + random(10000)
+        const email = username + '@me.com'
+
+        chai.request(URI)
+          .post('/users/')
+          .send({
+            username,
+            email,
+            password: 'password'
+          })
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(201)
+            done()
+          })
+      })
+    })
+
+    // PUT
+    describe('PUT users/:id', () => {
+      it('Should return a status 200', done => {
+        chai.request(URI)
+          .put(`/users/${id}`)
+          .send({
+            password: 'updatedPassword'
+          })
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            done()
+          })
+      })
+    })
+
+    // DELETE
+    describe('DELETE users/:id', () => {
+      it('Should return a status 200', done => {
+        chai.request(URI)
+          .delete(`/users/${id}`)
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            done()
+          })
+      })
+    })
 })
